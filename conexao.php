a17a318f38c1a0851755293b7aa7b632da50e820
Igor Teles Caetano <igor.caetano@123milhas.com.br>
2023-06-30 16:22:45 -0300

    cria uma tabelça e adiciona um aluno ao banco

--- conexao.php
+++ conexao.php
@@ -1,6 +1,8 @@
 <?php
 //cria a instancia de PDO - 1º parametro = string de coneção. Depois do dois pontos, informo as configurações do banco,s e for MySQl eu informaria mysql:host=endereco_do_servidor;dbname=nome_do_banco. Como é um banco de dados SQLite, só preciso informar o caminho.
-$caminhoBanco = __DIR__ . '/banco.sqlite'; //a constante DIR pega meu diretorio atual.
-$pdo = new PDO('sqlite:' . $caminhoBanco);
+$databasePath = __DIR__ . '/banco.sqlite'; //a constante DIR pega meu diretorio atual.
+$pdo = new PDO('sqlite:' . $databasePath);
 
 echo 'Conectei';
+
+$pdo->exec('CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);');
---
+++ inserir-aluno.php
@@ -0,0 +1,14 @@
+<?php
+
+use Alura\Pdo\Domain\Model\Student;
+
+require_once 'vendor/autoload.php';
+
+$databasePath = __DIR__ . '/banco.sqlite'; //a constante DIR pega meu diretorio atual.
+$pdo = new PDO('sqlite:' . $databasePath);
+
+$student = new Student(null, 'Igor Teles', new DateTimeImmutable('1993-03-06')); //crio um opjeto aluno no mundo da aplicação
+
+$sqlInsert = "INSERT INTO students(name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');"; //crio um opjeto aluno no mundo do banco de dados
+
+var_dump($pdo->exec($sqlInsert)); //executa uma instrução SQL e retorna o número de linhas afetadas.
